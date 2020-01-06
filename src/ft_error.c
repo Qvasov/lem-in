c64fa093553f52418723c26270280f2bb73312c6
Qvasov <Kvasovmi@yandex.ru>
2020-01-06 23:37:06 +0300

    ft_error

--- src/ft_error.c
+++ src/ft_error.c
@@ -6,27 +6,16 @@
 /*   By: dbennie <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/18 17:44:00 by dbennie           #+#    #+#             */
-/*   Updated: 2019/10/18 17:44:01 by dbennie          ###   ########.fr       */
+/*   Updated: 2020/01/06 23:35:31 by dbennie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lemin.h"
 
-static void	set_err_status(char *err)
-{
-	ft_strcpy(err, "no links");
-	ft_strcpy(err + 20, "no links");
-	return ;
-}
-
 void	ft_error(int id)
 {
-	char err_status[5][20];
-
-	set_err_status(&err_status[0][0]);
-	//
 	write (1, "ERROR", 5);
-	exit(1);
+	exit(id);
 }
 
 void ft_perror()
